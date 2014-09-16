5f2f7656dae128a5a594641caddab2a3f21f4b26
Florian Werner <fwerner13@gmail.com>
2014-09-16 15:44:51 +0200

    add view and tag array

--- TagcloudWidget.php
+++ TagcloudWidget.php
@@ -3,12 +3,23 @@
 namespace zeitgeist13\tagcloud;
 
 /**
- * This is just an example.
+ * @var array Array of tags
+ * 
+ * Example: 
+ * $tags = [
+ *      ['title' => 'cookies' , 'route' => ['food/cookies'], 'weight' => '40'],
+ *      ['title' => 'fruit' , 'route' => ['food/fruit'], 'weight' => '12'],
+ * ]
+ */
+public $tags;
+
+/**
+ * Renders a tagcloud based on model input
  */
 class TagcloudWidget extends \yii\base\Widget
 {
     public function run()
     {
-        return "Hello!";
+        return $this->render('index');
     }
 }
