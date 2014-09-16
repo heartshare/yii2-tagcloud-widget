2943199a9da75be232309896baf2b165aa074a3b
Florian Werner <fwerner13@gmail.com>
2014-09-16 15:46:36 +0200

    fix wrong placement of tag property

--- TagcloudWidget.php
+++ TagcloudWidget.php
@@ -3,21 +3,22 @@
 namespace zeitgeist13\tagcloud;
 
 /**
- * @var array Array of tags
- * 
- * Example: 
- * $tags = [
- *      ['title' => 'cookies' , 'route' => ['food/cookies'], 'weight' => '40'],
- *      ['title' => 'fruit' , 'route' => ['food/fruit'], 'weight' => '12'],
- * ]
- */
-public $tags;
-
-/**
  * Renders a tagcloud based on model input
  */
 class TagcloudWidget extends \yii\base\Widget
 {
+    /**
+     * @var array Array of tags
+     * 
+     * Example: 
+     * $tags = [
+     *      ['title' => 'cookies' , 'route' => ['food/cookies'], 'weight' => '40'],
+     *      ['title' => 'fruit' , 'route' => ['food/fruit'], 'weight' => '12'],
+     * ]
+     */
+    public $tags;
+
+    
     public function run()
     {
         return $this->render('index');
