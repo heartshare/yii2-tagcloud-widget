1d1048cc4c044d9a487babc8bfff83a1ffcc83da
Florian Werner <fwerner13@gmail.com>
2014-09-16 15:28:05 +0200

    add tagcloud widget class

---
+++ TagcloudWidget.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace zeitgeist13\tagcloud;
+
+/**
+ * This is just an example.
+ */
+class TagcloudWidget extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
